1d76abe3b1583398f38890332b482cf51488f793
KimKyeongSeog <kimks9241@naver.com>
2023-11-15 10:06:21 +0900

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 const App = () => {
-  return <div className="bg-red-100">Chat GPT!</div>;
+  return (
+  <div className="min-h-screen max-w-screen-md mx-auto flex">
+    <div className="bg-yellow-100 w-52">Side</div>
+    <div className="grow flex flex-col">
+      <div className="bg-blue-100 grow">Main</div>
+      <div className="bg-gray-100 h-24">Chat Bar</div>
+    </div>
+  </div>
+  
+  
+  );
 };
 
 export default App;
